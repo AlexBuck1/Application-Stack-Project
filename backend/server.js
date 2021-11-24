485f70f1b49f0af1c725209c4fcd15029b2e03f7
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-11-23 18:35:07 -0800

    connected mongo database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+
+//how we're going to communicate with mongo
 const mongoose = require("mongoose");
 
 //loads from dotenv file for safety
@@ -8,6 +10,17 @@ require("dotenv").config();
 //Creates express server and sends to port
 const app = express();
 const port = process.env.PORT || 5000;
+
+//This is the database and connecting to it
+const uri = process.env.URI;
+//
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+
+//once the connection is established, send a message to the console that it has been established
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
 
 //Middleware
 app.use(cors());
