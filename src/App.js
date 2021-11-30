69d5d3f942cc9ad15739b9364482aa3f554dd017
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-11-29 18:01:33 -0800

    fixing host issues

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" exact component={InterestList} />
         <Route path="/edit/:id" component={EditList} />
         <Route path="/create" component={CreateInterest} />
-        <Route path="/users" component={CreateUser} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </>
   );
